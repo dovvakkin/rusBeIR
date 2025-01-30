c8579ded80b09821e378e8bfdbb07bc7bb3bca8e
dovvakkin <dovvakkin@gmail.com>
2025-01-30 23:03:40 +0300

    add base rag generator

---
+++ rag/generator/base.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from utils.type_hints import RetrieverResults, GeneratorResults, Corpus, Queries, QueryPromptMaker
+
+def default_prompt_maker(
+    query_id: str,
+    retriever_results: RetrieverResults,
+    corpus: Corpus,
+    queries: Queries
+) -> str:
+    query_doc_ids = [i[0] for i in sorted(retriever_results[query_id].items(), key=lambda x: x[1], reverse=True)]
+    query_text = queries[query_id]
+    
+    return f"""
+Context:
+{'\n\n'.join([corpus[doc_id]["text"] for doc_id in query_doc_ids])}
+
+Query: {query_text}
+
+Your answer:
+    """
+    
+class BaseGenerator(ABC):
+
+    @abstractmethod
+    def generate(
+        self, 
+        retriever_results: RetrieverResults,
+        corpus: Corpus,
+        queries: Queries,
+        query_prompt_maker: QueryPromptMaker,
+        **kwargs
+    ) -> GeneratorResults:
+        pass
