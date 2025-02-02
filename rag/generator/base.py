2745a0b1e56a89334e8539acbaf3788ab3325082
dovvakkin <dovvakkin@gmail.com>
2025-02-02 22:20:29 +0300

    fix backslash in f-string for older pythons

--- rag/generator/base.py
+++ rag/generator/base.py
@@ -9,10 +9,13 @@ def default_prompt_maker(
 ) -> str:
     query_doc_ids = [i[0] for i in sorted(retriever_results[query_id].items(), key=lambda x: x[1], reverse=True)]
     query_text = queries[query_id]
+
+    # hack: python3.10 does not support backslash in f-string
+    double_newline = '\n\n'
     
     return f"""
 Context:
-{'\n\n'.join([corpus[doc_id]["text"] for doc_id in query_doc_ids])}
+{double_newline.join([corpus[doc_id]["text"] for doc_id in query_doc_ids])}
 
 Query: {query_text}
 
